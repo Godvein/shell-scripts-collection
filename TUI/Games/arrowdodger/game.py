d3a1f4085cc49923cb475e61eda0103013debd1d
Ankit Dware <sabindware@gmail.com>
2025-10-30 20:55:06 +0545

    added sprite and animation for hero with screen border

--- TUI/Games/arrowdodger/game.py
+++ TUI/Games/arrowdodger/game.py
@@ -3,31 +3,47 @@ from curses import wrapper
 from hero import Hero
 
 def main(stdscr):
+    # remove cursor
+    curses.curs_set(0)
+
     # initialize hero class
     hero = Hero(10, 10)
 
     # hero color
     curses.start_color()
-    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_GREEN)
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     hero_color = curses.color_pair(1)
 
     # game running
     running = True
 
+    # game over 
+    game_over = False
+
     stdscr.timeout(30) # run loop every 30ms
    
     while running:
+        while not game_over:
+            key = stdscr.getch()
+
+            game_over = hero.move(key, stdscr) # move hero and check of out of bounds ( returns true if out of screen )
+            hero.update()
+
+            # draw on screen
+            stdscr.clear()
+            hero.draw(stdscr, hero_color)
+            stdscr.refresh()
+
+            #logic to quit game
+            if key == ord('q'):
+                game_over = True
+
 
         key = stdscr.getch()
-        hero.move(key)
-
-        stdscr.clear()
-        hero.draw(stdscr, hero_color)
-        stdscr.refresh()
-
-        #logic to quit game
+        # game quit logic
         if key == ord('q'):
             running = False
+
     
 if __name__ == "__main__":
     wrapper(main)
--- TUI/Games/arrowdodger/hero.py
+++ TUI/Games/arrowdodger/hero.py
@@ -1,27 +1,77 @@
-import curses
+import os
 
 class Hero:
-    velocity = 1;
-    asciisprite = "@@"
 
     def __init__(self, x, y):
+        # 2d vector location
         self.x = x
         self.y = y
+        self.velocity = 1
+        # state for animation
+        self.state = "idle"
 
-    def move(self, key):
+        #animation frames
+        self.frames = self.load_frames()
+        self.frame_index = 0
+        self.tick = 0
+
+    def load_frames(self):
+        default_sprite = [
+                [
+                " o ",
+               "/|\\",
+               " /\\"
+                ],
+                ]
+        if self.state == "idle":
+            folder = os.path.join(os.path.dirname(__file__), "sprites/idle_hero") # gives absolute path to idle animation folder
+            frames = [] # initialize empty frames array
+
+        # loop the listed and sorted files in animation folder path
+            for file in sorted(os.listdir(folder)):
+                if file.startswith("idle"):
+                # open with absolute path to the animatio .txt file as f
+                    with open(os.path.join(folder, file)) as f: 
+                        frames.append(f.read().splitlines()) # add the txt animation to frames array
+
+        # now frames is 2d array
+            return frames 
+        return default_sprite
+
+    def move(self, key, stdscr):
+        screen_height, screen_width = stdscr.getmaxyx()
         # move down
         if key == ord('w'):
             self.y = self.y - self.velocity
+            if self.y < 1:
+                return True
         # move up
         if key == ord('s'):
             self.y = self.y + self.velocity
+            if self.y > screen_height-5:
+                return True
         # move left
         if key == ord('a'):
             self.x = self.x - self.velocity
+            if self.x < 1:
+                return True
         # move right 
         if key == ord('d'):
             self.x = self.x + self.velocity
+            if self.x > screen_width-5:
+                return True
+
+        return False
+
+    def update(self):
+
+        self.tick += 1
+        # increase frame index every 10 times the function is called
+        if self.tick % 10 == 0:
+            self.frame_index = (self.frame_index + 1) % len(self.frames)
 
     def draw(self, stdscr, hero_color):
-        stdscr.addstr(self.y, self.x, self.asciisprite, hero_color)
+        frame = self.frames[self.frame_index] #get a array from 2d array frames according to index
+        for i, line in enumerate(frame): # draw each array of frame y + i
+            stdscr.addstr(self.y + i, self.x, line, hero_color)
 
